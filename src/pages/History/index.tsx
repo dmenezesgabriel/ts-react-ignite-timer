d4a8caf9c15f8a6eb0f64eee1532f3cbb2d1d7c2
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-12-09 19:21:46 -0300

    feat: status component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -21,7 +21,9 @@ export function History() {
                 <td>Tarefa</td>
                 <td>20 Minutos</td>
                 <td>Ha 2 meses</td>
-                <td>Concluído</td>
+                <td>
+                  <Status statusColor="green">Concluído</Status>
+                </td>
               </tr>
             ))}
           </tbody>
